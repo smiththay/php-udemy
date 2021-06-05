8466b3ddfb2b79033aa921fcdd9848b777a67310
Smith Thay <contact.smith.thay@gmail.com>
2021-06-05 15:20:06 -0400

    practice section 2

--- _practical_app/2.php
+++ _practical_app/2.php
@@ -1,21 +1,34 @@
 <?php include "functions.php" ?>
 <?php include "includes/header.php" ?>
 
-	<section class="content">
+<section class="content">
 
-		<aside class="col-xs-4">
+	<aside class="col-xs-4">
 
-	<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
+		<?php Navigation(); ?>
 
 
-		<article class="main-content col-xs-8">
-		
+	</aside>
+	<!--SIDEBAR-->
+
+
+	<article class="main-content col-xs-8">
+
 
 
 		<?php
+
+
+		$number1 = 10;
+		$number2 = 20;
+
+		echo $number1 + $number2 . '<br>';
+
+		$array_1 = [10, 20];
+		$array_2 = ["number1" => 10, "number2" => 20];
+
+		echo $array_1[1] . '<br>';
+		echo $array_2["number1"];
 
 		/* Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
 
@@ -31,13 +44,14 @@
 
 			 */
 
-		
+
 
 
 		?>
 
-	
 
-		</article><!--MAIN CONTENT-->
 
-<?php include "includes/footer.php" ?>
+	</article>
+	<!--MAIN CONTENT-->
+
+	<?php include "includes/footer.php" ?>
